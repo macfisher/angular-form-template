1b0ef90692ba21e5a064638c88b5aa04128e1a20
Mac Fisher <macfisher01@gmail.com>
2015-04-24 15:51:46 -0600

    angular basic form template 2

---
+++ basicForm2/app.js
@@ -0,0 +1,15 @@
+//app.js
+
+//create Angular app
+var validationApp = angular.module('validationApp', []);
+
+//create Angular controller
+validationApp.controller('mainController', function($scope) {
+	//function to submit the form after all validation has occurred
+	$scope.submitForm = function(isValid) {
+		//check to make sure the form is completely valid
+		if (isValid) {
+			alert('Our form is VALID');
+		}
+	};
+});
